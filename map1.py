332602fcafec6e7b57968b24ba8f15be967151cb
Daniel Krzeminski <dankrzeminski32@gmail.com>
2021-04-27 23:05:18 -0500

    added html popups on markers

--- map1.py
+++ map1.py
@@ -5,7 +5,24 @@ data = pandas.read_csv("Volcanoes.txt")
 
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
 
+def color_el(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000<= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+    
+
+html = """
+Volcano name: <br> 
+<a href = "https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
 map = folium.Map(location=[38.58, -99.09], zoom_start = 6, tiles = "Stamen Terrain")
 #map object from folium package
 
@@ -13,8 +30,9 @@ map = folium.Map(location=[38.58, -99.09], zoom_start = 6, tiles = "Stamen Terra
 fg = folium.FeatureGroup(name = "My Map")
 
 #for-loop to add multiple points on map from volcanoes.txt file 
-for lt, ln in zip(lat, lon):
-    map.add_child(folium.Marker(location = [lt,ln], popup="Hi I am a Marker", icon = folium.Icon(color='green')))
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html = html % (name, name, el), width = 200, height = 100)
+    fg.add_child(folium.Marker(location = [lt, ln], popup = folium.Popup(iframe), icon = folium.Icon(color = color_el(el))))
 
 
 
